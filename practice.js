21569356ecc9933d49841686b7c429774d6f6518
ali <kemal_tar@hotmail.com>
2022-12-14 21:33:54 -0600

    otp method added

--- practice.js
+++ practice.js
@@ -1,54 +1,54 @@
-const users = {
-    Alex: {
-      email: 'alex@alex.com',
-      skills: ['HTML', 'CSS', 'JavaScript'],
-      age: 20,
-      isLoggedIn: false,
-      points: 30
-    },
-    Asab: {
-      email: 'asab@asab.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-      age: 25,
-      isLoggedIn: false,
-      points: 50
-    },
-    Brook: {
-      email: 'daniel@daniel.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-      age: 30,
-      isLoggedIn: true,
-      points: 50
-    },
-    Daniel: {
-      email: 'daniel@alex.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    },
-    John: {
-      email: 'john@john.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-      age: 20,
-      isLoggedIn: true,
-      points: 50
-    },
-    Thomas: {
-      email: 'thomas@thomas.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    },
-    Paul: {
-      email: 'paul@paul.com',
-      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-      age: 20,
-      isLoggedIn: false,
-      points: 40
-    }
-  }
+// const users = {
+//     Alex: {
+//       email: 'alex@alex.com',
+//       skills: ['HTML', 'CSS', 'JavaScript'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 30
+//     },
+//     Asab: {
+//       email: 'asab@asab.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 25,
+//       isLoggedIn: false,
+//       points: 50
+//     },
+//     Brook: {
+//       email: 'daniel@daniel.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//       age: 30,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     Daniel: {
+//       email: 'daniel@alex.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     John: {
+//       email: 'john@john.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//       age: 20,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     Thomas: {
+//       email: 'thomas@thomas.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     Paul: {
+//       email: 'paul@paul.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     }
+//   }
 
 //   console.log(users.Alex.skills.length
 //     )
@@ -74,7 +74,18 @@ const users = {
 // }
 // console.log(capab)
 
-for (var key in users) {
+// for (var key in users) {
     
-    console.log(key + " : " + users[key].skills)
-    }
+//     console.log(key + " : " + users[key].skills)
+//     }
+
+function generateOTP(limit){
+let digits='0123456789'
+let OTP=''
+for(let i=0;i<limit;i++){
+  OTP+=digits[Math.floor(Math.random()*10)]
+}
+return OTP
+}
+
+console.log(generateOTP(10))
