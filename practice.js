49eab4ae9bc506629ba852e87d0400bcbb27da30
ali <kemal_tar@hotmail.com>
2022-12-15 22:18:02 -0600

    file changed

--- practice.js
+++ practice.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // const users = {
 //     Alex: {
 //       email: 'alex@alex.com',
@@ -137,3 +138,146 @@ for(country of map){
 
 
 
+=======
+// const users = {
+//     Alex: {
+//       email: 'alex@alex.com',
+//       skills: ['HTML', 'CSS', 'JavaScript'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 30
+//     },
+//     Asab: {
+//       email: 'asab@asab.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 25,
+//       isLoggedIn: false,
+//       points: 50
+//     },
+//     Brook: {
+//       email: 'daniel@daniel.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+//       age: 30,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     Daniel: {
+//       email: 'daniel@alex.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     John: {
+//       email: 'john@john.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+//       age: 20,
+//       isLoggedIn: true,
+//       points: 50
+//     },
+//     Thomas: {
+//       email: 'thomas@thomas.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     },
+//     Paul: {
+//       email: 'paul@paul.com',
+//       skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+//       age: 20,
+//       isLoggedIn: false,
+//       points: 40
+//     }
+//   }
+
+//   console.log(users.Alex.skills.length
+//     )
+
+
+
+//   const iterate = (obj) => {
+//     Object.keys(obj).forEach(key => {
+//         console.log(`key: ${key}, value: ${obj[key]}`)
+//         if (typeof obj[key] === "object") {
+//             iterate(obj[key])
+//         }
+//     })
+// }
+
+
+ 
+// const capab={}
+// for(const [key,{skills}] of Object.entries(users)){
+//     capab[skills]=key
+    
+  
+// }
+// console.log(capab)
+
+// for (var key in users) {
+    
+//     console.log(key + " : " + users[key].skills)
+//     }
+
+// function generateOTP(limit){
+// let digits='0123456789'
+// let OTP=''
+// for(let i=0;i<limit;i++){
+//   OTP+=digits[Math.floor(Math.random()*10)]
+// }
+// return OTP
+// }
+
+// console.log(generateOTP(10))
+
+// 
+// let sum=0;
+// const numbers=[1,2,3,4,5];
+// numbers.forEach(num=>{
+//   sum=sum+num
+// })
+// console.log(sum)
+
+// const countries=['Finland','Denmark','Germany','France','Norway'];
+// countries.forEach(element=>{
+//   console.log(element.toUpperCase())
+// })
+
+// 
+
+
+// const names=['ali','yunus','ilyas','ahmet'];
+// const newnames=names.map((uppercase)=>uppercase.toUpperCase())
+//  console.log(newnames)
+
+// countries=[
+//     ['Finland','Helsinki'],
+//     ['Sweden','Stockholm'],
+//     ['Norway','Oslo']
+
+// ]
+
+// const map=new Map(countries);
+// console.log(map);
+// console.log(map.size);
+
+// countries=[
+//     ['Finland','Helsinki'],
+//     ['Sweden','Stockholm'],
+//     ['Norway','Oslo']
+
+// ];
+// const map=new Map(countries);
+
+for(country of map){
+    console.log(country)
+}
+
+const map=new Map(countries);
+
+
+
+
+
+>>>>>>> Stashed changes
